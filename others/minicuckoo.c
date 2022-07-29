046c2881b35556dea6e4dd9741ab5a299ad292cf
Widianto Nur Firmansah <xnaltasee@gmail.com>
2022-07-29 14:56:39 +0700

    minicuckoo: max limit to INT_MAX
    so we can store hash table entries up to 2^31

--- others/minicuckoo.c
+++ others/minicuckoo.c
@@ -1,8 +1,8 @@
+#include <limits.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
 
 #define ISPOWOF2(a)	((a) > 0 && !((a) & ((a) - 1)))
 #define GETIDX(a, n)	((a) & ((n) - 1))
@@ -16,37 +16,37 @@ typedef struct {
 
 typedef struct {
 	hashnode *node;
-	ssize_t count;
-	ssize_t cap;
+	int count;
+	int cap;
 } hashtable;
 
-static inline ssize_t hashfunc1(char *key)
+static inline unsigned int hashfunc1(char *key)
 {
-	ssize_t res = 0x1505;
+	unsigned int res = 0x1505U;
 	size_t l = strlen(key);
 
 	while (l--)
-		res = ((res << 5)) ^ *key++;
+		res = ((res << 5U)) ^ *key++;
 
 	return res;
 }
 
-static inline ssize_t hashfunc2(char *key)
+static inline unsigned int hashfunc2(char *key)
 {
-	ssize_t res = 0x811c9dc5;
+	unsigned int res = 0x811c9dc5U;
 	size_t l = strlen(key);
 
 	for (size_t i = 0; i < l; i++) {
 		res ^= key[i];
-		res *= 0x1000193;
+		res *= 0x1000193U;
 	}
 
 	return res;
 }
 
-int init(hashtable *h, ssize_t cap)
+int init(hashtable *h, unsigned int cap)
 {
-	cap = cap < 64 ? 64 : cap > 131072 ? 131072 : cap;
+	cap = cap < 64 ? 64 : cap > INT_MAX ? INT_MAX : (int)cap;
 	if (!ISPOWOF2(cap))
 		return -1;
 
@@ -61,10 +61,11 @@ int init(hashtable *h, ssize_t cap)
 	return 0;
 }
 
-ssize_t lookup(hashtable *h, char *key)
+int lookup(hashtable *h, char *key)
 {
 	hashnode *node = h->node;
-	ssize_t idx, hash, cap = h->cap >> 1;
+	unsigned int hash;
+	int idx, cap = h->cap >> 1;
 
 	hash = hashfunc1(key);
 	idx = GETIDX(hash, cap);
@@ -79,11 +80,12 @@ ssize_t lookup(hashtable *h, char *key)
 	return -1;
 }
 
-ssize_t insert(hashtable *h, char *key, void *value)
+int insert(hashtable *h, char *key, void *value)
 {
 	char *tk, *tv;
-	ssize_t (*hashfunc)(char *) = hashfunc1;
-	ssize_t idx, hash, cap = h->cap >> 1, maxloop = cap >> 1;
+	unsigned int (*hashfunc)(char *) = hashfunc1;
+	unsigned int hash;
+	int idx, cap = h->cap >> 1, maxloop = cap >> 1;
 
 	idx = lookup(h, key);
 	if (idx != -1)
@@ -125,7 +127,7 @@ ssize_t insert(hashtable *h, char *key, void *value)
 
 void delete(hashtable *h, char *key)
 {
-	ssize_t idx = lookup(h, key);
+	int idx = lookup(h, key);
 	if (idx == -1)
 		return;
 
@@ -135,7 +137,7 @@ void delete(hashtable *h, char *key)
 
 void clear(hashtable *h)
 {
-	for (ssize_t i = 0; i < h->cap; i++)
+	for (int i = 0; i < h->cap; i++)
 		h->node[i].occupied = false;
 
 	h->count = 0;
@@ -155,9 +157,9 @@ int main(void)
 		return 1;
 
 	h = &ht;
-	printf("cap: %zu, table cap: %zu\n", h->cap, h->cap >> 1);
+	printf("cap: %d, table cap: %d\n", h->cap, h->cap >> 1);
 
-	ssize_t idx[10];
+	int idx[10];
 	char *key[10] = {"aa","bb","cc","dd","ee","ff","gg","hh","ii","jj"};
 	int val[10] = {1,2,3,4,5,6,7,8,9,0};
 
@@ -167,20 +169,20 @@ int main(void)
 			printf("%s failed\n", key[i]);
 	}
 
-	printf("%zu\n", h->count);
+	printf("%d\n", h->count);
 
 	for (int i = 0; i < 10; i++) {
-		ssize_t _idx = lookup(h, key[i]);
+		int _idx = lookup(h, key[i]);
 		if (_idx != -1 && _idx == idx[i])
-			printf("%s OK (%zd)\n", key[i], idx[i]);
+			printf("%s OK (%d)\n", key[i], idx[i]);
 	}
 
 	for (int i = 0; i < 5; i++)
 		delete(h, key[i]);
 
-	printf("%zu\n", h->count);
+	printf("%d\n", h->count);
 	clear(h);
-	printf("%zu\n", h->count);
+	printf("%d\n", h->count);
 
 	destroy(h);
 	return 0;
